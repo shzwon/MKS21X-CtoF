560c478b3c68904d598a84274af3dff7d0ea959f
shzwon <rwu5@stuy.edu>
2018-09-20 00:05:50 -0400

    upload of file

---
+++ CtoFTester.java
@@ -0,0 +1,20 @@
+public boolean CtoFTester(int a, boolean b){
+
+}
+
+public int celsiusToFahrenheit(int tempF){
+
+}
+
+public int fahrenheitToCelsius(int tempC){
+
+}
+
+
+//-Create a java program with a class name of CtoFTester.java
+//-Create two functions, celsiusToFahrenheit and fahrenheitToCelsius
+//-BEFORE Writing the code, think about the following questions:
+//    -What should the functions have as parameter(s) ? What type should they be?
+//    -What should the functions return?
+//-Compile and run as you write this, using a main function to run and test your two functions.
+//-Commit any time you make progress towards your goal!
