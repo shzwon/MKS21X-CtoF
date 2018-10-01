a444f24fbae4e453c498cf2063eaa3e6ad582896
shzwon <rwu5@stuy.edu>
2018-10-01 17:42:19 -0400

    final push

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,30 +1,30 @@
-import java.util.Scanner;
+public class CtoFTester{
 
-public boolean CtoFTester(int a, int b){
-  System.out.println("enter 0 for C-to-F \n enter 1 for F-to-C");
-  Scanner b = new Scanner (System.in);
-  long a;
-  if (b == 0);
-    System.out.println (celsiusToFahrenheit(a));
-  else
-    System.out.println (fahrenheitToCelsius(a));
-}
+  public static double celsiusToFahrenheit(double c){
+    return (c * 1.8 +32.0);
+  }
 
-public int celsiusToFahrenheit(int tempC){
-  long tempC = 0.0;
-  return tempC = tempC * 1.8 + 32;
-}
 
-public int fahrenheitToCelsius(int tempF){
-  long tempF = 0.0;
-  return tempF = (tempF - 32) / 1.8
+  public static double fahrenheitToCelsius(double f){
+    return ((f - 32.0) /1.8);
+  }
+
+public static void main(String[] args) {
+  System.out.println(celsiusToFahrenheit(3.0));
+  System.out.println(celsiusToFahrenheit(12.0));
+  System.out.println(fahrenheitToCelsius(32.0));
+  System.out.println(fahrenheitToCelsius(48.0));
 }
 
 
-//-Create a java program with a class name of CtoFTester.java
-//-Create two functions, celsiusToFahrenheit and fahrenheitToCelsius
-//-BEFORE Writing the code, think about the following questions:
-//    -What should the functions have as parameter(s) ? What type should they be?
-//    -What should the functions return?
-//-Compile and run as you write this, using a main function to run and test your two functions.
-//-Commit any time you make progress towards your goal!
+}
+
+
+/* -Create a java program with a class name of CtoFTester.java
+-Create two functions, celsiusToFahrenheit and fahrenheitToCelsius
+-BEFORE Writing the code, think about the following questions:
+    -What should the functions have as parameter(s) ? What type should they be?
+    -What should the functions return?
+-Compile and run as you write this, using a main function to run and test your two functions.
+ -Commit any time you make progress towards your goal!
+*/
